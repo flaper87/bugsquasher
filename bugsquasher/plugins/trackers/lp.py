b508b0c4775747a4e950454452bfb375577a860f
Flaper Fesp <flaper87@gmail.com>
2013-05-23 00:17:18 +0200

    Added support for show / list commands

--- bugsquasher/plugins/trackers/lp.py
+++ bugsquasher/plugins/trackers/lp.py
@@ -1,21 +1,23 @@
-try:
-    from launchpadlib.launchpad import Launchpad
-except ImportError:
-    print "launchpadlib is missing."
+import os
+
+from termcolor import colored
+from launchpadlib import launchpad
 
 from bugsquasher.utils import jsonutils
 
 
 class LP(object):
 
+    local_file = ".launchpad.json"
+
     @staticmethod
     def lp(config, section):
         cache = section.get("cache_dir",
                             "~/.bugsquasher/cache")
 
-        return Launchpad.login_anonymously('bugsquasher',
-                                           'production',
-                                           cache)
+        return launchpad.Launchpad.login_anonymously('bugsquasher',
+                                                     'production',
+                                                     cache)
 
     @classmethod
     def on_take(cls, config, section, bug):
@@ -27,5 +29,34 @@ class LP(object):
             "description": bug.description
         }
 
-        with open('.launchpad.json', 'wab') as f:
+        for msg in bug.messages:
+            comment = {
+                "author": msg.owner.name,
+                "content": msg.content,
+                "time": msg.date_created.isoformat(),
+            }
+            summary.setdefault("comments", []).append(comment)
+
+        with open(cls.local_file, 'wab') as f:
             f.write(jsonutils.dumps(summary, ensure_ascii=True, indent=4))
+
+    @classmethod
+    def on_show(cls, config, section, **kwargs):
+        if os.path.exists(cls.local_file):
+            with open(cls.local_file, "rb") as f:
+                summary = jsonutils.loads(f.read())
+
+            print colored("Bug Title: ", 'green') + summary["title"]
+            #print "\tStatus: %s" % summary["status"]
+            print "\t%s comments" % len(summary["comments"])
+
+            if config.verbose:
+                for idx, comment in enumerate(summary["comments"]):
+                    print colored("\tComment: %s" % (idx + 1), 'yellow')
+                    header = "\tAuthor: %s, Date %s" % \
+                             (comment["author"], comment["time"])
+                    print colored(header, 'yellow')
+                    print "\t%s" % comment["content"].replace("\n", "\n\t")
+                    print "\t"
+
+    on_list = on_show
